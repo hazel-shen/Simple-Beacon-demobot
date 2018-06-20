4e8db53bcdf5aa2fd0a381868b8be4096e6c8b10
hazel <hazel.shen@linecorp.com>
2018-06-20 16:37:41 +0800

    initial commit

---
+++ src/main/java/com/linecorp/spoon/SpoonApplication.java
@@ -0,0 +1,11 @@
+package com.linecorp.spoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpoonApplication.class, args);
+	}
+}
