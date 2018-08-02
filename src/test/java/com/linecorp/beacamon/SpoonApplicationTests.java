b63a644c83953c747f2746e67a09df49e351d987
hazel <hazel.shen@linecorp.com>
2018-08-02 21:03:24 +0800

    Remove test

--- src/test/java/com/linecorp/beacamon/SpoonApplicationTests.java
+++ src/test/java/com/linecorp/beacamon/SpoonApplicationTests.java
@@ -1,19 +1,12 @@
 package com.linecorp.beacamon;
 
 
-import com.linecorp.beacamon.controller.BotController;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class SpoonApplicationTests {
 
-	@Test
+//	@Test
 	public void contextLoads() {
 		//assertThat(BotController.class).isNotNull();
 	}
